02caf379daa9edc2196799ab17c2c352dee4464f
Alejandro <alejandrofernandezm94@gmail.com>
2021-02-25 22:16:02 +0100

    chore: add source Allow * Origin

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     async headers() {
       return [
         {
+          source: "*",
           headers: [
             { key: "Access-Control-Allow-Origin", value: "*" },
           ]
