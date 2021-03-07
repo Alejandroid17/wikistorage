775ae5e1f0d69db02c9b5877b9ce5e0211cc084c
Alejandro <alejandrofernandezm94@gmail.com>
2021-03-07 22:37:46 +0100

    chore: add GitHubLink component

---
+++ components/Link/GitHubLink.js
@@ -0,0 +1,11 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const GitHubLink = () => {
+
+    return <a href="https://github.com/Alejandroid17/wikistorage">
+        <FontAwesomeIcon icon={faGithub} size="2x" className='' />
+    </a>
+}
+
+export default GitHubLink;
