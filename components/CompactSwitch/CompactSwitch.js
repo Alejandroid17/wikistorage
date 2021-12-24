28429d13a99280ce573c21f8ce6823c01b84e29c
Alejandro <alejandrofernandezm94@gmail.com>
2021-12-25 00:38:11 +0100

    chore: migrate CompactSwitch component

--- components/CompactSwitch/CompactSwitch.js
+++ components/CompactSwitch/CompactSwitch.js
@@ -1,22 +1,49 @@
-import { IconButton } from "@chakra-ui/react"
-import { faTh, faThList } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useContext } from "react"
 import { GlobalContext } from "../../contexts/GlobalContext"
 
 const CompactSwitch = () => {
   const [state, dispatch] = useContext(GlobalContext)
 
-  const icon = <FontAwesomeIcon icon={state.compact ? faTh : faThList} />
-
   return (
-    <IconButton
-      w="100%"
-      p={1.5}
-      variant="ghost"
-      onClick={() => dispatch({ type: "TOGGLE_COMPACT" })}
-      icon={icon}
-    />
+    <button onClick={() => dispatch({ type: "TOGGLE_COMPACT" })}>
+      {state.compact ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-layout-grid"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <rect x="4" y="4" width="6" height="6" rx="1"></rect>
+          <rect x="14" y="4" width="6" height="6" rx="1"></rect>
+          <rect x="4" y="14" width="6" height="6" rx="1"></rect>
+          <rect x="14" y="14" width="6" height="6" rx="1"></rect>
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-layout-list"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <rect x="4" y="4" width="16" height="6" rx="2"></rect>
+          <rect x="4" y="14" width="16" height="6" rx="2"></rect>
+        </svg>
+      )}
+    </button>
   )
 }
 
