2e42af984e8480bbc4c216dc41dbb9bb03c505f7
Alejandro <alejandrofernandezm94@gmail.com>
2021-12-25 19:12:08 +0100

    chore: migrate header component

--- components/Header/MainHeader/MainHeader.js
+++ components/Header/MainHeader/MainHeader.js
@@ -1,14 +1,12 @@
 import dynamic from "next/dynamic"
 
-import { Flex, HStack } from "@chakra-ui/react"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faBoxOpen, faSearch } from "@fortawesome/free-solid-svg-icons"
+import SettingMenu from "../../Menus/SettingMenu"
 
-const CompactSwitch = dynamic(import("../../CompactSwitch/CompactSwitch"))
 const GitHubLink = dynamic(import("../../Link/GitHubLink"))
-const SearchInput = dynamic(import("../../SearchInput"))
-const ThemeSwitch = dynamic(import("../../ThemeSwitch/ThemeSwitch"))
+const SearchInput = dynamic(import("../../SearchInput/SearchInput"))
 const BaseHeader = dynamic(import("../BaseHeader/BaseHeader"))
 const TitleHeader = dynamic(import("../TitleHeader/TitleHeader"))
 
@@ -17,21 +15,18 @@ library.add(faBoxOpen, faSearch, faGithub)
 const MainHeader = ({ onSearch }) => {
   return (
     <BaseHeader>
-      <Flex w="100%" h="100%" px="6" align="center" justify="space-between">
-        <Flex width="25%" justify="flex-start">
-          <TitleHeader />
-        </Flex>
-        <Flex width="50%" justify="center">
-          <SearchInput onChange={onSearch} />
-          <HStack spacing={1} px={2} display={{ base: "none", md: "flex" }}>
-            <CompactSwitch size={"lg"} />
-            <ThemeSwitch size={"lg"} />
-          </HStack>
-        </Flex>
-        <Flex width="25%" justify="flex-end">
-          <GitHubLink />
-        </Flex>
-      </Flex>
+      <div className="flex w-1/3 overflow-hidden lg:w-1/5">
+        <TitleHeader />
+      </div>
+      <div className="flex gap-1 overflow-hidden w-3/3 sm:w-2/3 lg:w-3/5">
+        <SearchInput onChange={onSearch} />
+        <div className="">
+          <SettingMenu />
+        </div>
+      </div>
+      <div className="flex items-center justify-end w-1/3 pl-8 lg:w-1/5">
+        <GitHubLink href="https://github.com/Alejandroid17/wikistorage" />
+      </div>
     </BaseHeader>
   )
 }
--- components/Header/TitleHeader/TitleHeader.js
+++ components/Header/TitleHeader/TitleHeader.js
@@ -1,29 +1,32 @@
-import { Box, Heading, HStack, useBreakpointValue } from "@chakra-ui/react"
-import { faBoxOpen } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useContext } from "react"
 import { GlobalContext } from "../../../contexts/GlobalContext"
 
 const TitleHeader = () => {
   const [, dispatch] = useContext(GlobalContext)
-  const showTitle = useBreakpointValue({ sm: false, md: false, lg: true })
 
   return (
-    <HStack
-      onClick={() => dispatch({ type: "TOGGLE_DRAWER" })}
-      cursor={"pointer"}
-    >
-      <Box w="40px">
-        <FontAwesomeIcon icon={faBoxOpen} />
-      </Box>
-      {showTitle && (
-        <Box>
-          <Heading as="h1" size="lg">
-            WikiStorage
-          </Heading>
-        </Box>
-      )}
-    </HStack>
+    <div className="flex items-center gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="mb-1 icon icon-tabler icon-tabler-book"
+        width="30"
+        height="30"
+        viewBox="0 0 24 24"
+        strokeWidth="1"
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0"></path>
+        <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0"></path>
+        <line x1="3" y1="6" x2="3" y2="19"></line>
+        <line x1="12" y1="6" x2="12" y2="19"></line>
+        <line x1="21" y1="6" x2="21" y2="19"></line>
+      </svg>
+      <div className="hidden text-xl sm:inline">WikiStorage</div>
+    </div>
   )
 }
 
