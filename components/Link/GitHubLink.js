35d9aba07c35db183e60c73011385522d820a18a
Alejandro <alejandrofernandezm94@gmail.com>
2021-12-25 19:13:46 +0100

    chore: migrate GitHubLink component

--- components/Link/GitHubLink.js
+++ components/Link/GitHubLink.js
@@ -1,18 +1,25 @@
-import { Link } from "@chakra-ui/react"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
-const GitHubLink = ({ linkText, showIcon = true }) => {
+const GitHubLink = ({ href, linkText, showIcon = true }) => {
   return (
-    <Link
-      href="https://github.com/Alejandroid17/wikistorage"
-      target={"blank"}
-      aria-label="GitHub"
-      w={34}
-    >
-      {showIcon && <FontAwesomeIcon icon={faGithub} size="2x" />}
+    <a href={href} target={"blank"} aria-label="GitHub">
+      {showIcon && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-brand-github"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+        </svg>
+      )}
       {linkText}
-    </Link>
+    </a>
   )
 }
 
