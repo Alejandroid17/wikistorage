bb5ea6b6a26ec94086cee856cd129647c7044be4
Alejandro <alejandrofernandezm94@gmail.com>
2021-12-25 19:13:26 +0100

    chore: migrate CompactSwitch component

--- components/CompactSwitch/CompactSwitch.js
+++ components/CompactSwitch/CompactSwitch.js
@@ -1,47 +1,53 @@
 import { useContext } from "react"
 import { GlobalContext } from "../../contexts/GlobalContext"
 
-const CompactSwitch = () => {
+const CompactSwitch = ({ iconClassName }) => {
   const [state, dispatch] = useContext(GlobalContext)
 
   return (
     <button onClick={() => dispatch({ type: "TOGGLE_COMPACT" })}>
       {state.compact ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-layout-grid"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <rect x="4" y="4" width="6" height="6" rx="1"></rect>
-          <rect x="14" y="4" width="6" height="6" rx="1"></rect>
-          <rect x="4" y="14" width="6" height="6" rx="1"></rect>
-          <rect x="14" y="14" width="6" height="6" rx="1"></rect>
-        </svg>
+        <div className="flex gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`icon icon-tabler icon-tabler-layout-grid ${iconClassName}`}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <rect x="4" y="4" width="6" height="6" rx="1"></rect>
+            <rect x="14" y="4" width="6" height="6" rx="1"></rect>
+            <rect x="4" y="14" width="6" height="6" rx="1"></rect>
+            <rect x="14" y="14" width="6" height="6" rx="1"></rect>
+          </svg>
+          <span>Layout grid</span>
+        </div>
       ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-layout-list"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <rect x="4" y="4" width="16" height="6" rx="2"></rect>
-          <rect x="4" y="14" width="16" height="6" rx="2"></rect>
-        </svg>
+        <div className="flex gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`icon icon-tabler icon-tabler-layout-list ${iconClassName}`}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <rect x="4" y="4" width="16" height="6" rx="2"></rect>
+            <rect x="4" y="14" width="16" height="6" rx="2"></rect>
+          </svg>
+          <span> Layout list</span>
+        </div>
       )}
     </button>
   )
